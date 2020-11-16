a20bc7757db8b5807ac389d0f2ac76bfaff948b3
prakashgun <mail.prakash.india@gmail.com>
2020-11-16 07:58:00 +0530

    Context added

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function IndexScreen() {
+    return (
+        <View>
+            <Text>IndexScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
